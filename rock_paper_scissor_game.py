0dcc84214f0d1a8db54783a5b6980e3aa4049d01
Dianne <crisdiannecasino2@gmail.com>
2026-03-04 19:53:20 +0800

    working rock paper scissor game with hand detector

--- rock_paper_scissor_game.py
+++ rock_paper_scissor_game.py
@@ -4,8 +4,6 @@ from cvzone.HandTrackingModule import HandDetector
 import time
 import random
 
-from matplotlib.backends.backend_ps import papersize
-
 capture = cv2.VideoCapture(0)
 capture.set(3, 640)
 capture.set(4, 480)
@@ -15,6 +13,7 @@ detector = HandDetector(maxHands=1)
 timer = 0
 state_result = False
 start_game = False
+scores = [0, 0] # [AI score, Player score]
 
 while True:
     background_image = cv2.imread("images/background_image.png")
@@ -59,12 +58,26 @@ while True:
                     image_ai = cv2.imread(f'images/{ai_move}_image.png', cv2.IMREAD_UNCHANGED)
                     background_image = cvzone.overlayPNG(background_image, image_ai, (149, 310))
 
-                    print(player_move)
+                    # Player Wins
+                    if (player_move == "rock" and ai_move == "scissor") or \
+                            (player_move == "paper" and ai_move == "rock") or \
+                            (player_move == "scissor" and ai_move == "paper"):
+                        scores[1] += 1
+
+                    # AI Wins
+                    if (ai_move == "rock" and player_move == "scissor") or \
+                            (ai_move == "paper" and player_move == "rock") or \
+                            (ai_move == "scissor" and player_move == "paper"):
+                        scores[0] += 1
 
     background_image[233:653, 795:1195] = image_scaled
 
     if state_result:
         background_image = cvzone.overlayPNG(background_image, image_ai, (149, 310))
+
+    cv2.putText(background_image, str(scores[0]), (410, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
+    cv2.putText(background_image, str(scores[1]), (1112, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
+    cv2.putText(background_image, "Press 's' to play", (570, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
     # cv2.imshow("Image", image)
     cv2.imshow("Background", background_image)
