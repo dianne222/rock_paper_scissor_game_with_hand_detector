965effd25a4a9a9b0b2f49a331dfb4022ab47c89
Dianne <crisdiannecasino2@gmail.com>
2026-03-04 18:48:03 +0800

    added hand detector

--- source_code/rock_paper_scissor_game.py
+++ source_code/rock_paper_scissor_game.py
@@ -1,9 +1,12 @@
 import cv2
 import cvzone
+from cvzone.HandTrackingModule import HandDetector
 
 capture = cv2.VideoCapture(0)
 capture.set(3, 640)
 capture.set(4, 480)
+
+detector = HandDetector(maxHands=1)
 
 while True:
     background_image = cv2.imread("images/background_image.png")
@@ -15,6 +18,9 @@ while True:
 
     image_scaled = cv2.resize(image, (0, 0), None, 0.875, 0.875)
     image_scaled = image_scaled[:, 80:480]
+
+    # Find Hands
+    hands, img = detector.findHands(image_scaled, draw=True, flipType=True)
 
     background_image[233:653, 795:1195] = image_scaled
 
